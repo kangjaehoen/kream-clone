671c160dac79e84ea92aede3caaeaafb7f58bc3e
hong <gjsdms1244@gmail.com>
2026-03-09 13:09:12 +0900

    DataSource, JPA, Redis auto-config for startup without DB/Redis 임시 비활성화

--- kream/src/main/java/com/clone/kream/KreamApplication.java
+++ kream/src/main/java/com/clone/kream/KreamApplication.java
@@ -2,8 +2,20 @@ package com.clone.kream;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.jpa.JpaRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernateJpaAutoConfiguration;
 
-@SpringBootApplication
+/**
+ * 임시지거으로 datasource, jpa, redis 자동 설정 비활성화
+ */
+@SpringBootApplication(exclude = {
+	DataSourceAutoConfiguration.class,
+	HibernateJpaAutoConfiguration.class,
+	JpaRepositoriesAutoConfiguration.class,
+	RedisAutoConfiguration.class
+})
 public class KreamApplication {
 
 	public static void main(String[] args) {
